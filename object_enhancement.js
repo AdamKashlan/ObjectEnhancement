d5cb9dfceaed30c30e22e3c784c8d40e24f90a08
adam <akashlan03@gmail.com>
2022-06-05 14:07:18 -0400

    adding js file

---
+++ object_enhancement.js
@@ -0,0 +1,38 @@
+function createInstructor(firstName, lastName) {
+    return {
+        firstName,
+        lastName
+    }
+}
+
+let favoriteNumber = 42;
+
+const instructor = {
+  firstName: "Colt",
+  [favoriteNumber]: "That is my favorite!"
+}
+
+let instructor = {
+    firstName: "Colt",
+    sayHi() {
+    	return "Hi!";
+    },
+		sayBye() {
+			return this.firstName + " says bye!";
+		}
+}
+
+const d = createAnimal("dog", "bark", "Woof!")
+d.bark()
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+s.bleet()
+
+function createAnimal(species, verb, noise) {
+	return {
+		species,
+		[verb]() {
+			return noise;
+		}
+	}
+}
